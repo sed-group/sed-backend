885ecdce4d7ba9f71590d07ba7febb16199c7f5a
johnmartins <johnmartins1992@gmail.com>
2021-04-27 17:16:03 +0200

    Implemented blanket login control in core router

--- apps/core/router.py
+++ apps/core/router.py
@@ -1,16 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Security
 
 from apps.core.users.router import router as router_users
 from apps.core.authentication.router import router as router_auth
+from apps.core.authentication.utils import verify_token
 from apps.core.applications.router import router as router_apps
 from apps.core.projects.router import router as router_projects
 
 router = APIRouter()
 
-router.include_router(router_users, prefix='/users', tags=['users'])
 router.include_router(router_auth, prefix='/auth', tags=['authentication'])
-router.include_router(router_apps, prefix='/apps', tags=['applications'])
-router.include_router(router_projects, prefix='/projects', tags=['projects'])
+router.include_router(router_users, prefix='/users', tags=['users'], dependencies=[Security(verify_token)])
+router.include_router(router_apps, prefix='/apps', tags=['applications'], dependencies=[Security(verify_token)])
+router.include_router(router_projects, prefix='/projects', tags=['projects'], dependencies=[Security(verify_token)])
 
 
 @router.get("/", summary="Core API root", description="This is pointless", tags=['core'])
