0366b84e028ba2f5170b74fbbcd6bf9360c7721a
johnmartins <johnmartins1992@gmail.com>
2021-05-27 16:29:22 +0200

    Application data is now read from a file instead of DB
    This ensures that, during development, duplicate IDs for applications are unlikely.

--- apps/core/applications/router.py
+++ apps/core/applications/router.py
@@ -1,6 +1,8 @@
+from typing import List
+
 from fastapi import APIRouter, Security
 
-from apps.core.applications.implementation import impl_get_apps, impl_post_app, impl_get_app, impl_delete_app
+from apps.core.applications.implementation import impl_get_apps, impl_get_app
 from apps.core.authentication.utils import verify_token
 from apps.core.applications.models import Application
 
@@ -10,34 +12,22 @@ router = APIRouter()
 @router.get("/",
             summary="Lists all applications",
             description="Produces a list of applications in alphabetical order",
+            response_model=List[Application],
             dependencies=[Security(verify_token)])
-async def get_apps(segment_length: int, index: int):
+async def get_apps() -> List[Application]:
     """
 
     :param segment_length: Sample size. Min=1.
     :param index: Offset, multiplied by segment_length. Min=0 (no offset).
     :return:
     """
-    return impl_get_apps(segment_length, index)
-
-
-@router.post("/",
-             summary="Insert new application",
-             dependencies=[Security(verify_token, scopes=['admin'])])
-async def post_app(app: Application):
-    return impl_post_app(app)
+    return impl_get_apps()
 
 
 @router.get("/{app_id}",
             summary="Get application by ID",
             description="Produces a list of applications in alphabetical order",
+            response_model=Application,
             dependencies=[Security(verify_token)])
-async def get_app(app_id: int):
+async def get_app(app_id: str) -> Application:
     return impl_get_app(app_id)
-
-
-@router.delete("/{app_id}",
-               summary="Remove an application",
-               dependencies=[Security(verify_token, scopes=['admin'])])
-async def delete_app(app_id: int):
-    return impl_delete_app(app_id)
