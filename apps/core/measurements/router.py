81a6ad614ea2887d0b6b6f7dd21eab087a4aefe9
johnmartins <johnmartins1992@gmail.com>
2021-05-31 16:16:44 +0200

    Improved measurement API docs

--- apps/core/measurements/router.py
+++ apps/core/measurements/router.py
@@ -10,27 +10,32 @@ router = APIRouter()
 
 
 @router.get("/{measurement_id}",
-             summary="Get measurement",
-             description="Get measurement information")
+            summary="Get measurement",
+            description="Get measurement information",
+            response_model=MeasurementMdls.Measurement)
 async def get_measurement (measurement_id: int):
     return MeasurementImpl.impl_get_measurement(measurement_id)
 
 
 @router.post("/",
-             summary="Post measurement")
+             summary="Post measurement",
+             response_model=MeasurementMdls.Measurement)
 async def post_measurement(measurement: MeasurementMdls.MeasurementPost):
     return MeasurementImpl.impl_post_measurement(measurement)
 
 
-@router.get("/{measurement_id}/{measurement_result_data_id}",
-            summary="Get measurement result datapoint")
+@router.get("/{measurement_id}/results/{measurement_result_data_id}",
+            summary="Get measurement result datapoint",
+            response_model=MeasurementMdls.MeasurementResultData)
 async def get_measurement_result_by_id(measurement_id: int, measurement_result_data_id: int):
     return MeasurementImpl.impl_get_measurement_result_by_id(measurement_id, measurement_result_data_id)
 
 
-@router.get("/{measurement_id}/",
+@router.get("/{measurement_id}/results",
             summary="Get measurement result data",
-            description="Search for specific data measurement. Dates are provided as UNIX timestamp in milliseconds.")
+            description="Search for specific data measurement. Dates are provided as UNIX timestamp in milliseconds.",
+            
+            response_model=List[MeasurementMdls.MeasurementResultData])
 async def get_measurement_results(measurement_id: int,
                                   dtype: Optional[MeasurementMdls.MeasurementDataType] = None,
                                   date_from: Optional[int] = None,
@@ -42,7 +47,8 @@ async def get_measurement_results(measurement_id: int,
     return MeasurementImpl.impl_get_measurement_results(measurement_id, dtype, date_class, date_from, date_to)
 
 
-@router.post("{measurement_id}/",
-             summary="Post measurement result data")
+@router.post("/{measurement_id}/results/",
+             summary="Post measurement result data",
+             response_model=None)
 async def post_measurement_result(measurement_id: int, measurement_data_post: MeasurementMdls.MeasurementResultDataPost):
     MeasurementImpl.impl_post_measurement_result(measurement_id, measurement_data_post)
