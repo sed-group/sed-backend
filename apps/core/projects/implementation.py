ca49f1613a1ee49509befddf6f1eb424aa793f8f
johnmartins <johnmartins1992@gmail.com>
2021-08-25 13:18:28 +0200

    Fixed project-module import structure

--- apps/core/projects/implementation.py
+++ apps/core/projects/implementation.py
@@ -1,136 +1,144 @@
+from typing import Optional, List
+
 from fastapi import HTTPException, status
 
 from apps.core.applications.exceptions import ApplicationNotFoundException
-from apps.core.projects.storage import *
+import apps.core.projects.storage as storage
 from apps.core.db import get_connection
-from apps.core.projects.models import AccessLevel, ProjectPost
+import apps.core.projects.models as models
+import apps.core.projects.exceptions as exc
 
 
 def impl_get_projects(segment_length: int, index: int):
     with get_connection() as con:
-        return db_get_projects(con, segment_length, index)
+        return storage.db_get_projects(con, segment_length, index)
 
 
 def impl_get_user_projects(user_id: int, segment_length: int = 0, index: int = 0):
     with get_connection() as con:
-        return db_get_user_projects(con, user_id, segment_length=segment_length, index=index)
+        return storage.db_get_user_projects(con, user_id, segment_length=segment_length, index=index)
 
 
 def impl_get_project(project_id: int):
     try:
         with get_connection() as con:
-            return db_get_project(con, project_id)
-    except ProjectNotFoundException:
+            return storage.db_get_project(con, project_id)
+    except exc.ProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Project not found",
         )
 
 
-def impl_post_project(project: ProjectPost):
+def impl_post_project(project: models.ProjectPost) -> models.Project:
     with get_connection() as con:
-        res = db_post_project(con, project)
+        res = storage.db_post_project(con, project)
         con.commit()
         return res
 
 
-def impl_delete_project(project_id: int):
+def impl_delete_project(project_id: int) -> bool:
     try:
         with get_connection() as con:
-            res = db_delete_project(con, project_id)
+            res = storage.db_delete_project(con, project_id)
             con.commit()
             return res
-    except ProjectNotFoundException:
+    except exc.ProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Project not found",
         )
 
 
-def impl_post_participant(project_id: int, user_id: int, access_level: AccessLevel):
+def impl_post_participant(project_id: int, user_id: int, access_level: models.AccessLevel) -> bool:
     try:
         with get_connection() as con:
-            res = db_add_participant(con, project_id, user_id, access_level)
+            res = storage.db_add_participant(con, project_id, user_id, access_level)
             con.commit()
             return res
-    except ProjectNotFoundException:
+    except exc.ProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Project not found",
         )
 
 
-def impl_delete_participant(project_id: int, user_id: int):
+def impl_delete_participant(project_id: int, user_id: int) -> bool:
     try:
         with get_connection() as con:
-            res = db_delete_participant(con, project_id, user_id)
+            res = storage.db_delete_participant(con, project_id, user_id)
             con.commit()
             return res
-    except ProjectNotFoundException:
+    except exc.ProjectNotFoundException:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project not found",
+        )
+    except exc.ParticipantChangeException:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="No participant deleted. The participant could not be found."
+        )
+
+
+def impl_put_name(project_id: int, name: str) -> bool:
+    try:
+        with get_connection() as con:
+            res = storage.db_put_name(con, project_id, name)
+            con.commit()
+            return res
+    except exc.ProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Project not found",
         )
 
 
-def impl_put_name(project_id: int, name: str):
+def impl_post_subproject(subproject: models.SubProjectPost, current_user_id: int, project_id: Optional[int] = None) \
+        -> models.SubProject:
     try:
         with get_connection() as con:
-            res = db_put_name(con, project_id, name)
+            res = storage.db_post_subproject(con, subproject, current_user_id, project_id)
             con.commit()
             return res
-    except ProjectNotFoundException:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Project not found",
-        )
-
-
-def impl_post_subproject(subproject: SubProjectPost, current_user_id: int, project_id: Optional[int] = None) \
-        -> SubProject:
-    try:
-        with get_connection() as con:
-            res = db_post_subproject(con, subproject, current_user_id, project_id)
-            con.commit()
-            return res
-    except ProjectNotFoundException:
+    except exc.ProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Project not found"
         )
-    except SubProjectDuplicateException:
+    except exc.SubProjectDuplicateException:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="Subproject with same native id and application sid already exists."
         )
 
 
-def impl_get_subprojects(project_id: int) -> List[SubProject]:
+def impl_get_subprojects(project_id: int) -> List[models.SubProject]:
     with get_connection() as con:
-        return db_get_subprojects(con, project_id)
+        return storage.db_get_subprojects(con, project_id)
 
 
-def impl_get_subproject(project_id: int, subproject_id: int):
+def impl_get_subproject(project_id: int, subproject_id: int) -> models.SubProject:
     try:
         with get_connection() as con:
-            return db_get_subproject(con, project_id, subproject_id)
-    except SubProjectNotFoundException:
+            return storage.db_get_subproject(con, project_id, subproject_id)
+    except exc.SubProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Sub project not found"
         )
-    except ProjectNotFoundException:
+    except exc.ProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Project not found"
         )
 
 
-def impl_get_subproject_native(application_sid: str, native_project_id: int) -> SubProject:
+def impl_get_subproject_native(application_sid: str, native_project_id: int) -> models.SubProject:
     try:
         with get_connection() as con:
-            return db_get_subproject_native(con, application_sid, native_project_id)
-    except SubProjectNotFoundException:
+            return storage.db_get_subproject_native(con, application_sid, native_project_id)
+    except exc.SubProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Sub project not found"
@@ -142,17 +150,17 @@ def impl_get_subproject_native(application_sid: str, native_project_id: int) -> 
         )
 
 
-def impl_delete_subproject(project_id: int, subproject_id: int):
+def impl_delete_subproject(project_id: int, subproject_id: int) -> bool:
     try:
         with get_connection() as con:
-            db_delete_subproject(con, project_id, subproject_id)
+            storage.db_delete_subproject(con, project_id, subproject_id)
             con.commit()
-    except SubProjectNotFoundException:
+    except exc.SubProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Sub project not found"
         )
-    except ProjectNotFoundException:
+    except exc.ProjectNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Project not found"
