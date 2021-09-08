c41b37baf78085f35ce95f072a7b3f34dcc9621e
johnmartins <johnmartins1992@gmail.com>
2021-09-08 15:39:19 +0200

    serve-static
    Serve static for production environment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+import os.path as path
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 import main_router as api
 import setup
@@ -15,6 +19,13 @@ app = FastAPI(
 
 app.include_router(api.router, prefix="/api")
 
+# Production specific logic
+deploy_mode = os.getenv("SED_DEPLOY_MODE")
+if deploy_mode == 'prod':
+    # Set static content
+    static_content_directory = '/var/www/static'
+    if path.exists(static_content_directory):
+        app.mount("/static", StaticFiles(directory="/var/www/static"), name="static")
 
 # CORS
 origins = ["http://localhost:8080"]
@@ -25,6 +36,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
 
 # Misc middleware
 setup.install_middleware(app)
