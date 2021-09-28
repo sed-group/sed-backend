030970eca4cfa9616a866b1999558a4fa4b9d59d
thoskar <78076294+thoskar@users.noreply.github.com>
2021-09-28 11:45:38 +0200

    Nothing important

---
+++ apps/cvs/algorithms.py
@@ -0,0 +1,5 @@
+import apps.cvs.models as models
+
+
+def square_a_number(n: int) -> int:
+    return n * n
--- apps/cvs/router.py
+++ apps/cvs/router.py
@@ -2,13 +2,7 @@ from typing import List
 
 from fastapi import APIRouter, Security, Depends
 
-import apps.difam.implementation as impl
-import apps.difam.models as models
-from libs.datastructures.pagination import ListChunk
-from apps.core.users.models import User
-from apps.core.authentication.utils import get_current_active_user
-from apps.core.projects.dependencies import SubProjectAccessChecker
-from apps.core.projects.models import AccessLevel
+import algorithms
 
 router = APIRouter()
 
@@ -17,4 +11,4 @@ DIFAM_APP_SID = 'MOD.CVS'
 
 @router.get("/projects/", summary="Just testing")
 async def get_cvs_projects(n: int) -> str:
-    return f'{n}^2 = {n * n}'
+    return f'{n} * {n} = {algorithms.square_a_number(n)}'
