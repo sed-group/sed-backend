fd3ba20d287b80a52f2e6f8580d294005021be2f
Julian Martinsson <johnmartins1992@gmail.com>
2021-10-11 12:58:24 +0200

    Finalized users tests

--- tests/apps/core/users/test_users.py
+++ tests/apps/core/users/test_users.py
@@ -1,4 +1,5 @@
 import apps.core.users.implementation as users_impl
+import apps.core.users.exceptions as users_exc
 import tests.testutils as testutils
 
 
@@ -73,3 +74,68 @@ def test_get_user_me(client, std_headers):
     # Assert
     assert res.status_code == 200
     assert res.json()["username"] is not None
+
+
+def test_get_users_unauthenticated(client):
+    # Act
+    res = client.get("/api/core/users/users")
+    # Assert
+    assert res.status_code == 401
+
+
+def test_post_user_unauthenticated(client):
+    # Act
+    res = client.post("/api/core/users/",
+                      json={
+                          "username": testutils.random_str(3, 40),
+                          "email": testutils.random_str(3, 40) + "@gmail.com",
+                          "full_name": testutils.random_str(3, 40) + " " + testutils.random_str(3, 40),
+                          "disabled": False,
+                          "password": testutils.random_str(8, 40),
+                          "scopes": "user"
+                      })
+    # Assert
+    assert res.status_code == 401
+
+
+def test_get_me_unauthenticated(client):
+    # Act
+    res = client.get("/api/core/users/me")
+    # Assert
+    assert res.status_code == 401
+
+
+def test_get_user_unauthenticated(client):
+    # Setup
+    user = testutils.random_user_post(admin=False, disabled=False)
+    new_user = users_impl.impl_post_user(user)
+
+    # Act
+    res = client.get(f"/api/core/users/{new_user.id}")
+
+    # Assert
+    assert res.status_code == 401
+
+    # Cleanup
+    users_impl.impl_delete_user_from_db(new_user.id)
+
+
+def test_delete_user_unauthenticated(client):
+    # Setup
+    user = testutils.random_user_post(admin=False, disabled=False)
+    new_user = users_impl.impl_post_user(user)
+
+    # Act
+    res = client.delete(f"/api/core/users/{new_user.id}")
+    user_exists = True
+    user_check = users_impl.impl_get_user_with_id(new_user.id)
+
+    if user_check is None:
+        user_exists = False
+
+    # Assert
+    assert res.status_code == 401
+    assert user_exists is True
+
+    # Cleanup
+    users_impl.impl_delete_user_from_db(new_user.id)
