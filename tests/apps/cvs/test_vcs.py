23f305c2c985dacd9454d624a71ec12e206ddf14
Eh Uh <erik.00.berg@gmail.com>
2022-05-02 12:23:30 +0200

    Refactoring of testing

--- tests/apps/cvs/test_vcs.py
+++ tests/apps/cvs/test_vcs.py
@@ -11,53 +11,6 @@ import tests.apps.cvs.testutils as tu
 import tests.testutils as testutils
 import sedbackend.apps.core.users.implementation as impl_users
 
-def test_create_cvs_project(client, admin_headers):
-    # Act 
-    res = client.post("/api/cvs/project/create",
-                    headers=admin_headers,
-                    json={
-                        "name":  testutils.random_str(3, 30),
-                        "description": testutils.random_str(20, 200)
-                    })
-    assert res.status_code == 200
-
-    # cleanup
-    impl.delete_cvs_project(res.json()["id"], res.json()["owner"]["id"])
-
-def test_delete_cvs_project(client, std_headers, std_user):
-    #Setup 
-    current_user = impl_users.impl_get_user_with_username(std_user.username)
-    project = tu.seed_random_project(current_user.id)
-
-    # Act
-
-    res = client.delete(f'/api/cvs/project/{project.id}/delete', headers=std_headers)
-
-    # Assert
-    assert res.status_code == 200
-
-
-
-def test_edit_cvs_project(client, std_headers, std_user):
-    #Setup 
-    current_user = impl_users.impl_get_user_with_username(std_user.username)
-    project = tu.seed_random_project(current_user.id)
-
-    #Act
-    name = testutils.random_str(5,50)
-    res = client.put(f'/api/cvs/project/{project.id}/edit', 
-                        headers=std_headers,
-                        json = {
-                            "name": name,
-                            "description": testutils.random_str(20, 200)
-                        })
-    
-    # Assert
-    assert res.status_code == 200
-    assert res.json()["name"] == name
-
-    #Cleanup
-    tu.delete_project_by_id(project.id, current_user.id)
 
 def test_get_VCS(client, std_headers, std_user):
     # Setup 
@@ -162,49 +115,4 @@ def test_delete_vcs(client, std_headers, std_user):
     res.status_code = 200
 
     #Cleanup
-    tu.delete_project_by_id(project.id, current_user.id)
-
-def test_create_value_driver_no_unit(client, std_headers, std_user):
-    #Setup
-    current_user = impl_users.impl_get_user_with_username(std_user.username)
-    project = tu.seed_random_project(current_user.id)
-
-    #Act
-    name = testutils.random_str(5,50)
-    res = client.post(f'api/cvs/project/{project.id}/value-driver/create',
-                    headers = std_headers,
-                    json = {
-                        "name": name
-                    })
-    
-    # Assert
-    assert res.status_code == 200
-    assert res.json()["name"] == name
-
-    #cleanup
-    tu.delete_vd_by_id(res.json()["id"], project.id, current_user.id)
-    tu.delete_project_by_id(project.id, current_user.id)
-
-def test_create_value_driver_with_unit(client, std_headers, std_user):
-    #Setup
-    current_user = impl_users.impl_get_user_with_username(std_user.username)
-    project = tu.seed_random_project(current_user.id)
-
-    #Act
-    name = testutils.random_str(5,50)
-    unit = testutils.random_str(1,30)
-    res = client.post(f'api/cvs/project/{project.id}/value-driver/create',
-                    headers = std_headers,
-                    json = {
-                        "name": name,
-                        "unit": unit
-                    })
-    
-    # Assert
-    assert res.status_code == 200
-    assert res.json()["name"] == name
-    assert res.json()["unit"] == unit
-
-    #cleanup
-    tu.delete_vd_by_id(res.json()["id"], project.id, current_user.id)
     tu.delete_project_by_id(project.id, current_user.id)
