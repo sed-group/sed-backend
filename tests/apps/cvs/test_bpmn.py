e2b9d659c1bc6228766a1dc08485c11aef08045e
Eh Uh <erik.00.berg@gmail.com>
2022-05-05 16:40:21 +0200

    bpmn tests work except test_edit_bpmn

--- tests/apps/cvs/test_bpmn.py
+++ tests/apps/cvs/test_bpmn.py
@@ -58,5 +58,30 @@ def test_get_bpmn(client, std_headers, std_user):
    
 
 def test_edit_bpmn(client, std_headers, std_user):
-    pass
+    #Setup
+#    current_user = impl_users.impl_get_user_with_username(std_user.username)
+#    project = tu.seed_random_project(current_user.id)
+#    vcs = tu.seed_random_vcs(current_user.id, project.id)
+#    bpmn_nodes = tu.seed_random_bpmn_nodes(project.id, vcs.id, current_user.id, 20)
+#    amount_of_bpmn_nodes = len(bpmn_nodes)
+#    bpmn_edges = tu.seed_random_bpmn_edges(project.id, vcs.id, current_user.id, bpmn_nodes, 6)
 
+
+
+    #Act
+#    res = client.put(f'/api/cvs/project/{project.id}/vcs/{vcs.id}/bpmn/edit',
+#                    headers=std_headers,
+#                    json={
+#                        "nodes": bpmn_nodes,
+#                        "edges": bpmn_edges
+#                    })
+
+    #Assert
+#    assert res.status_code == 200
+
+    #Cleanup
+#    tu.delete_multiple_bpmn_edges(bpmn_edges, project.id, vcs.id, current_user.id)
+#    tu.delete_multiple_bpmn_nodes(bpmn_nodes, project.id, vcs.id, current_user.id)
+#    tu.delete_VCS_with_ids([vcs.id], project.id, current_user.id)
+#    tu.delete_project_by_id(project.id, current_user.id)
+    pass
