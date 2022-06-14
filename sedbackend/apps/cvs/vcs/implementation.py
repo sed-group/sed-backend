c3973225d0f719cbd4be4b6d3f4457b44c866dcf
EppChops <erik.00.berg@gmail.com>
2022-06-14 17:15:49 +0200

    Added error handling to create vcs table

--- sedbackend/apps/cvs/vcs/implementation.py
+++ sedbackend/apps/cvs/vcs/implementation.py
@@ -445,8 +445,20 @@ def create_vcs_table(new_table: models.VcsRowPost, vcs_id: int)-> bool:
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f'Could not find value driver with id={e.value_driver_id}.',
         )
+    except exceptions.ISOProcessNotFoundException:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='No such ISO process'
+        )
+    except exceptions.SubprocessNotFoundException:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='No such subprocess'
+        )
     except auth_ex.UnauthorizedOperationException:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail='Unauthorized user.',
         )
+
+
