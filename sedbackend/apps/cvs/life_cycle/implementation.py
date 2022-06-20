f82c22d1e1b4f0f510546af4b7577812028928e6
Oscar Bennet <oscar.bennet@hotmail.se>
2022-06-20 17:34:57 +0200

    error handling get and edit bpmn

--- sedbackend/apps/cvs/life_cycle/implementation.py
+++ sedbackend/apps/cvs/life_cycle/implementation.py
@@ -78,6 +78,11 @@ def update_node(node_id: int, node: models.NodePost) -> bool:
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f'Could not find node with id={node_id}.',
         )
+    except exceptions.NodeFailedToUpdateException:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Failed to update node.',
+        )
     except auth_ex.UnauthorizedOperationException:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -113,6 +118,16 @@ def update_bpmn(vcs_id: int, bpmn: models.BPMNGet) -> bool:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f'Could not find vcs with id={vcs_id}.',
+        )
+    except exceptions.NodeNotFoundException:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Could not find node.',
+        )
+    except exceptions.NodeFailedToUpdateException:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Failed to update node.',
         )
     except auth_ex.UnauthorizedOperationException:
         raise HTTPException(
