7dc2843b0ffe4a0219dd79856c16c407923a0897
EppChops <erik.00.berg@gmail.com>
2022-12-08 16:57:20 +0100

    Added test cases for project

--- sedbackend/apps/cvs/project/models.py
+++ sedbackend/apps/cvs/project/models.py
@@ -1,19 +1,19 @@
 from datetime import datetime
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sedbackend.apps.core.users.models import User
 
 
 class CVSProject(BaseModel):
     id: int
-    name: str
+    name: str = Field(..., max_length=255)
     description: Optional[str] = None
-    currency: Optional[str] = None
+    currency: Optional[str] = Field(None, max_length=10)
     owner: User
     datetime_created: datetime
 
 
 class CVSProjectPost(BaseModel):
-    name: str
+    name: str = Field(..., max_length=255)
     description: Optional[str] = None
-    currency: Optional[str] = None
+    currency: Optional[str] = Field(None, max_length=10)
--- sql/V220608_cvs.sql
+++ sql/V220608_cvs.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `seddb`.`cvs_projects`
     `id`               INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `name`             VARCHAR(255) NULL     DEFAULT 'Unnamed project',
     `description`      TEXT         NULL     DEFAULT NULL,
-    `currency`         VARCHAR(10)   NULL     DEFAULT '€',
+    `currency`         VARCHAR(10)   NULL    DEFAULT NULL,
     `owner_id`         INT UNSIGNED NOT NULL,
     `datetime_created` DATETIME(3)  NOT NULL DEFAULT NOW(3),
     PRIMARY KEY (`id`),
