7830fd516d33c81d5f8f3b2d213ce1971429c035
Oscar Bennet <oscar.bennet@hotmail.se>
2023-04-03 10:32:07 +0200

    parsing for formulas including vd and ef

--- tests/apps/cvs/simulation/test_sim_utils.py
+++ tests/apps/cvs/simulation/test_sim_utils.py
@@ -1,5 +1,6 @@
 from sedbackend.apps.cvs.simulation.storage import parse_formula
-from sedbackend.libs.parsing.expressions import get_prefix_names, replace_prefix_names
+from sedbackend.libs.parsing.expressions import get_prefix_names, get_prefix_variables, \
+    replace_prefix_variables
 from sedbackend.libs.parsing.parser import NumericStringParser
 
 
@@ -20,9 +21,11 @@ def test_parse_formula_simple():
 
 def test_parse_formula_values():
     # Setup
-    vd_values = [{"id": 1, "name": "Speed", "value": 3}, {"id": 2, "name": "Weight", "value": 4}]
-    mi_values = [{"id": 1, "name": "Test", "value": 5}, {"id": 2, "name": "Test2", "value": 6}]
-    formula = f'2*"VD(Speed [km/h])"+"EF(Test2 [T2])"'
+    vd_values = [{"id": 1, "name": "Speed", "unit": "km/h", "value": 3},
+                 {"id": 2, "name": "Weight", "unit": "kg", "value": 4}]
+    mi_values = [{"id": 1, "name": "Test", "unit": "T", "value": 5},
+                 {"id": 2, "name": "Test 2", "unit": "T2", "value": 6}]
+    formula = f'2*"VD(Speed [km/h])"+"EF(Test 2 [T2])"'
     nsp = NumericStringParser()
 
     # Act
@@ -33,29 +36,76 @@ def test_parse_formula_values():
     assert nsp.eval(new_formula) == 12
 
 
-def test_get_prefix_names():
+def test_parse_formula_vd_no_exist():
+    # Setup
+    vd_values = [{"id": 1, "name": "Speed", "unit": "km/h", "value": 3},
+                 {"id": 2, "name": "Weight", "unit": "kg", "value": 4}]
+    mi_values = [{"id": 1, "name": "Test", "unit": "T", "value": 5},
+                 {"id": 2, "name": "Test 2", "unit": "T2", "value": 6}]
+    formula = f'2*"VD(DontExist [km/h])"+"EF(Dont Exist [T2])"'
+    nsp = NumericStringParser()
+
+    # Act
+    new_formula = parse_formula(formula, vd_values, mi_values)
+
+    # Assert
+    assert new_formula == "2*0+0"
+    assert nsp.eval(new_formula) == 0
+
+
+def test_parse_formula_unit_no_exist():
+    # Setup
+    vd_values = [{"id": 1, "name": "Speed", "unit": None, "value": 3}]
+    mi_values = [{"id": 2, "name": "Test 2", "unit": None, "value": 6}]
+    formula = f'2*"VD(Speed [N/A])"+"EF(Test 2 [N/A])"'
+    nsp = NumericStringParser()
+
+    # Act
+    new_formula = parse_formula(formula, vd_values, mi_values)
+
+    # Assert
+    assert new_formula == "2*3+6"
+    assert nsp.eval(new_formula) == 12
+
+
+def test_get_prefix_variables():
     # Setup
     text_vd = f'2*"VD(Speed [km/h])"+"VD(Test [T])"'
-    text_mi = f'2*"EF(Test [T])"+"EF(Test2 [T2])"'
+    text_mi = f'2*"EF(Test (OK) [T])"+"EF(Test2 [T2])"'
+
+    # Act
+    variables_vd = get_prefix_variables("VD", text_vd)
+    variables_mi = get_prefix_variables("EF", text_mi)
+
+    # Assert
+    assert variables_vd == ["Speed [km/h]", "Test [T]"]
+    assert variables_mi == ["Test (OK) [T]", "Test2 [T2]"]
+
+
+def test_get_prefix_names():
+    # Setup
+    text_vd = f'2*"VD(Speed King [L] [km/h])"+"VD(Test(L) */&¢€ [T])"'
+    text_mi = f'2*"EF(Test (OK) [T])"+"EF(Test2 [T2])"'
 
     # Act
     names_vd = get_prefix_names("VD", text_vd)
     names_mi = get_prefix_names("EF", text_mi)
 
     # Assert
-    assert names_vd == ["Speed", "Test"]
-    assert names_mi == ["Test", "Test2"]
+    assert names_vd == ["Speed King [L]", "Test(L) */&¢€"]
+    assert names_mi == ["Test (OK)", "Test2"]
 
 
-def test_replace_prefix_names():
+def test_replace_prefix_variables():
     # Setup
     text_vd = f'2*"VD(Speed [km/h])"+"VD(Test [T])"'
-    text_mi = f'2*"EF(Test [T])"+"EF(Test2 [T2])"'
+    text_mi = f'2*"EF(Test (OK) [T])"+"EF(Test2 [T2])"'
 
     # Act
-    new_text_vd = replace_prefix_names("VD", "Speed", "2", text_vd)
-    new_text_mi = replace_prefix_names("EF", "Test", "4", text_mi)
+    new_text_vd = replace_prefix_variables("VD", "Speed [km/h]", "2", text_vd)
+    new_text_mi = replace_prefix_variables("EF", "Test (OK) [T]", "4", text_mi)
 
     # Assert
     assert new_text_vd == f'2*2+"VD(Test [T])"'
     assert new_text_mi == f'2*4+"EF(Test2 [T2])"'
+
