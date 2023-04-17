15050f407c61278f6578ae6a9d8c08ada0a2498a
Eh Uh <erik.00.berg@gmail.com>
2023-04-17 17:14:07 +0200

    Fixed problem where VD was deleted from multiple DG's when it should only be deleted from one

--- sedbackend/apps/cvs/design/storage.py
+++ sedbackend/apps/cvs/design/storage.py
@@ -144,7 +144,7 @@ def edit_design_group(db_connection: PooledMySQLConnection, project_id: int, des
         for vd_id in to_delete:
             delete_statement = MySQLStatementBuilder(db_connection)
             delete_statement.delete(DESIGN_GROUP_DRIVER_TABLE) \
-                .where('value_driver = %s', [vd_id]) \
+                .where('design_group = %s and value_driver = %s', [design_group_id, vd_id]) \
                 .execute(fetch_type=FetchType.FETCH_NONE)
 
     return get_design_group(db_connection, project_id, design_group_id)
