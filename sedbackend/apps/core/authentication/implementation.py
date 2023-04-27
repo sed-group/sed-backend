2a690fd0ae77737d11333f00c0f230ae10bc9dd1
johnmartins <johnmartins1992@gmail.com>
2023-04-27 16:14:09 +0200

    Added token renewal end-points

--- sedbackend/apps/core/authentication/implementation.py
+++ sedbackend/apps/core/authentication/implementation.py
@@ -1,9 +1,13 @@
+from datetime import timedelta
+
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import HTTPException, status
 
 from sedbackend.apps.core.authentication.exceptions import InvalidCredentialsException
-from sedbackend.apps.core.authentication.login import login
+from sedbackend.apps.core.authentication.login import login, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from sedbackend.apps.core.authentication.utils import parse_jwt_token, parse_scopes
 from sedbackend.apps.core.users.exceptions import UserDisabledException
+from sedbackend.apps.core.users.models import User
 
 
 def impl_login_with_token(form_data: OAuth2PasswordRequestForm):
@@ -20,4 +24,18 @@ def impl_login_with_token(form_data: OAuth2PasswordRequestForm):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="User has been suspended",
             headers={"WWW-Authenticate": "Bearer"}
+        )
+
+
+def impl_renew_token(user: User):
+    try:
+
+        return create_access_token(
+            data={"sub": user.username, "scopes": parse_scopes(user.scopes)},
+            expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        )
+    except UserDisabledException:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User has been suspended"
         )
