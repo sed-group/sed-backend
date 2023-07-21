c4b7b4c6d0bd484c70df107c94c6e490d681c4df
jyborn <jyborn@student.chalmers.se>
2023-07-21 11:33:47 +0200

    new file for sql

--- sql/V220608_cvs.sql
+++ sql/V220608_cvs.sql
@@ -150,20 +150,6 @@ CREATE TABLE IF NOT EXISTS `seddb`.`cvs_value_drivers`
         REFERENCES  `seddb`.`users`(`id`)
         ON DELETE CASCADE
 );
-# Value driver to project relation
-CREATE TABLE IF NOT EXISTS `seddb`.`cvs_project_value_drivers`
-(
-    `project`           INT UNSIGNED NOT NULL,
-    `value_driver`      INT UNSIGNED NOT NULL,
-    PRIMARY KEY (`project`, `value_driver`),
-    FOREIGN KEY (`project`)
-        REFERENCES `seddb`.`cvs_projects`(`id`)
-        ON DELETE CASCADE,
-    FOREIGN KEY (`value_driver`)
-        REFERENCES `seddb`.`cvs_value_drivers`(`id`)
-        ON DELETE CASCADE
-);
-CREATE UNIQUE INDEX `project_value_driver_index` ON `seddb`.`cvs_project_value_drivers`  (project, value_driver);
 
 #Vcs row and value driver connection
 CREATE TABLE IF NOT EXISTS `seddb`.`cvs_vcs_need_drivers`
