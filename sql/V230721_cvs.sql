fa6873769bdf848c7abb65266b758e4c547a3c48
jyborn <jyborn@student.chalmers.se>
2023-08-15 13:48:31 +0200

    updated sql file test

--- sql/V230721_cvs.sql
+++ sql/V230721_cvs.sql
@@ -1,7 +1,19 @@
 # Value driver to project relation
-ALTER TABLE `seddb`.`cvs_value_drivers`
-    ADD COLUMN `project_id` INT;
-
+DROP TABLE IF EXISTS `seddb`.`cvs_value_drivers`;
+CREATE TABLE IF NOT EXISTS `seddb`.`cvs_value_drivers`
+(
+    `id`                INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user`              INT UNSIGNED NOT NULL,
+    `name`              TEXT NOT NULL,
+    `unit`              VARCHAR(10) NULL,
+    `project_id`        INT
+    FOREIGN KEY(`user`)
+        REFERENCES  `seddb`.`users`(`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY(`project_id`)
+        REFERENCES `seddb`.`cvs_projects` (`id`)
+        ON DELETE CASCADE;
+);
 
 SET FOREIGN_KEY_CHECKS = 0;
 ALTER TABLE `seddb`.`cvs_subprocesses`
@@ -56,6 +68,9 @@ CREATE TABLE IF NOT EXISTS `seddb`.`cvs_formulas_value_drivers`
     FOREIGN KEY (`value_driver`)
         REFERENCES `seddb`.`cvs_value_drivers` (`id`)
         ON DELETE CASCADE,
+	FOREIGN KEY (`project`, `value_driver`)
+        REFERENCES `seddb`.`cvs_value_drivers` (`project_id`, `id`)
+        ON DELETE CASCADE
 );
 
 ALTER TABLE `seddb`.`cvs_design_mi_formulas`
